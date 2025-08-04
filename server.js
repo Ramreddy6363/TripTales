8d1a56b755bffc4d45184f25378249a526464d65
Your Name <ramreddy2557626@gmail.com>
2025-08-04 11:32:54 +0530

    Database setup

--- server.js
+++ server.js
@@ -1,7 +1,46 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
 const app = require(`./app`);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Db connecton successful');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, require: [true, 'A tour must have price'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The PARK CMPER',
+  rating: 4.6,
+  price: 5000,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log("Error:",err));
 const port = process.env.PORT;
 app.listen(port, '127.0.0.1', () => {
   console.log(`App running on port ${port}`);
